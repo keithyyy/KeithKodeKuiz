020aa949930f35aeedf73765e38f2a5180c1ece5
Keith Crooc <keith.crooc@gmail.com>
2020-10-27 22:03:53 -0400

    js for final score will save score into highScores Array

--- final.js
+++ final.js
@@ -3,6 +3,27 @@ const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem('highScores'));
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 finalScore.innerHTML = mostRecentScore;
+
+const max_score_listed = 10;
+
+username.addEventListener('keyup', () => {
+
+    saveHighScore = e => {
+        e.preventDefault();
+
+        const score = {
+            score: mostRecentScore,
+            name: username.value,
+        }  
+
+        highScores.push(score);
+        console.log(highScores)
+
+
+        localStorage.setItem('highScores', JSON.stringify(highScores));
+        window.location.assign("index.html");
+    }
+})
