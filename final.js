4b6085b93919f188f86de35ff565b08501b13c39
Keith Crooc <keith.crooc@gmail.com>
2020-10-27 22:30:14 -0400

    Added seperate JS for leaderboard to populate list

--- final.js
+++ final.js
@@ -2,8 +2,9 @@ const username = document.querySelector('#username');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScoresList = document.querySelector('#highScoresList')
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScores = JSON.parse(localStorage.getItem('highScores'));
 
 finalScore.innerHTML = mostRecentScore;
 
@@ -24,6 +25,6 @@ username.addEventListener('keyup', () => {
 
 
         localStorage.setItem('highScores', JSON.stringify(highScores));
-        window.location.assign("index.html");
+        window.location.assign("leaderboard.html");
     }
-})
+});
